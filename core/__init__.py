9dde29f43238ad0a60a29b9a947faf5230a527ca
RomanL91 <dev_roman91@mail.ru>
2024-05-20 03:02:56 +0600

    black formated

--- core/__init__.py
+++ core/__init__.py
@@ -2,7 +2,9 @@ __all__ = (
     "DataBaseManager",
     "db_manager",
     "settings",
+    "Base",
 )
 
 from .DB_manager import DataBaseManager, db_manager
 from .settings import settings
+from .BASE_model import Base
