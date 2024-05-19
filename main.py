2a972c7ffee369dc0bd665d2d70206066bcf90ad
RomanL91 <dev_roman91@mail.ru>
2024-05-20 01:58:00 +0600

    launch point

---
+++ main.py
@@ -0,0 +1,15 @@
+import uvicorn
+
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def start_test():
+    return {'message': "1, 2, 3 Start!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, reload=True)
