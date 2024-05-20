cb575df9039bd62d7da5e8053a55a4951fd4691c
RomanL <dev_roman91@mail.ru>
2024-05-21 04:35:07 +0700

    adding schemas

---
+++ auth_user_app/schemas.py
@@ -0,0 +1,45 @@
+from typing import Annotated
+from annotated_types import MinLen, MaxLen
+from pydantic import BaseModel, EmailStr, ConfigDict
+
+
+# ====================================================
+# Представлены базовые схемы для управления.
+# Для наглядности они унаследованы от BaseModel
+# и очень сильно похожи друг на друга, отличаясь
+# практически только именем класса.
+# ====================================================
+
+
+class CreateUserSchema(BaseModel):
+    username: Annotated[str, MinLen(3), MaxLen(20)]
+    password: Annotated[str, MinLen(4), MaxLen(20)]
+    email: EmailStr
+
+
+class ReadUserSchema(BaseModel):
+    model_config = ConfigDict(strict=True)
+
+    id: int
+    username: str
+    email: EmailStr | None = None
+
+
+class UpdateUserSchema(BaseModel):
+    model_config = ConfigDict(strict=True)
+
+    username: str
+    password: str
+
+    email: EmailStr | None = None
+    active: bool = True
+
+
+class UpdateUserPartialSchema(BaseModel):
+    model_config = ConfigDict(strict=True)
+
+    username: str | None = None
+    password: str | None = None
+
+    email: EmailStr | None = None
+    active: bool | None = None
