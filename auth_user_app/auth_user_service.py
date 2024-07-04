5c9eeeb3e1de243cbd14a0f29aaba6169c95ac70
RomanL91 <dev_roman91@mail.ru>
2024-07-04 15:19:56 +0500

    создание методов для работы с новым типом аутентификации (черновой вариант)

--- auth_user_app/auth_user_service.py
+++ auth_user_app/auth_user_service.py
@@ -1,3 +1,5 @@
+import string, secrets
+
 from core.BASE_unit_of_work import IUnitOfWork
 from auth_user_app.models import User
 from auth_user_app.schemas import (
@@ -6,6 +8,7 @@ from auth_user_app.schemas import (
     UpdateUserSchema,
     UpdateUserPartialSchema,
     JWT,
+    GoogleUserInfo,
 )
 from core.settings import SettingsAuth
 
@@ -64,7 +67,89 @@ class UserService:
             await uow.user.delete_obj(obj_id=user_id)
             await uow.commit()
 
-    # TODO вынести логику хеширования
+    async def get_or_create_user_and_generate_tokens(
+        self,
+        uow: IUnitOfWork,
+        user_data: GoogleUserInfo,
+        external_id_use: bool = False,
+    ) -> User:
+        user_data = GoogleUserInfo(**user_data)
+        async with uow:
+            # Попытка найти пользователя по имени пользователя
+            user = await uow.user.get_obj(id=user_data.id)
+            if external_id_use:
+                user = await uow.user.get_obj(external_id=user_data.id)
+            if user is None:
+                # Если пользователь не найден, создаем нового
+                user_dict = user_data.model_dump()
+                password = self.generate_random_password()
+                user_dict["password"] = self.password_hashed(password)
+                user_dict = self.transform_user_data_to_schema(user_dict)
+
+                try:
+                    user = await uow.user.create_obj(user_dict)
+                    await uow.commit()
+                except IntegrityError as e:
+                    # Если возникла ошибка при создании пользователя (например, конфликт имен)
+                    raise HTTPException(
+                        status_code=status.HTTP_400_BAD_REQUEST,
+                        detail="Пользователь с таким именем уже существует.",
+                    )
+            # TODO DRY! =====================
+            access_token = jwt_service.encode_jwt(
+                payload={
+                    "user_id": user.id,
+                    jwt_service.token_type_field: jwt_service.access_token_type,
+                }
+            )
+            refresh_token = jwt_service.encode_jwt(
+                payload={
+                    "user_id": user.id,
+                    jwt_service.token_type_field: jwt_service.refresh_token_type,
+                }
+            )
+            return JWT(
+                token_type=jwt_service.token_type,
+                access_token=access_token,
+                refresh_token=refresh_token,
+            )
+            # TODO DRY END! =====================
+
+    # TODO вынести логику хеширования/работы с паролем
+    def generate_random_password(self, length=12):
+        # Набор возможных символов включает буквы, цифры и некоторые специальные символы
+        characters = string.ascii_letters + string.digits + "!@#$%^&*()"
+        # Генерация безопасного случайного пароля
+        password = "".join(secrets.choice(characters) for _ in range(length))
+        return password
+
+    def transform_user_data_to_schema(self, raw_data: dict) -> dict:
+        # Проверка наличия необходимых ключей в исходных данных
+        if (
+            "id" not in raw_data
+            or "email" not in raw_data
+            or "password" not in raw_data
+        ):
+            raise ValueError("Недостаточно данных для создания пользователя")
+
+        # Принимаем 'name' для 'username', если он не задан, используем 'given_name' и 'family_name'
+        if "name" in raw_data:
+            username = raw_data["name"]
+        elif "given_name" in raw_data and "family_name" in raw_data:
+            username = f"{raw_data['given_name']} {raw_data['family_name']}"
+        else:
+            raise ValueError("Не найдены данные для имени пользователя")
+
+        # Создание словаря, соответствующего структуре CreateUserSchema
+        transformed_data = {
+            "external_id": raw_data["id"],
+            "username": username,
+            "password": raw_data["password"],
+            "email": raw_data["email"],
+        }
+
+        return transformed_data
+
     def password_hashed(self, password: str) -> str:
         hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
         return hashed_password.decode("utf-8")
@@ -86,6 +171,7 @@ class UserService:
                 raise error_403
         if not self.check_password(password=password, hashed_password=user.password):
             raise error_403
+        # TODO DRY! =====================
         access_token = jwt_service.encode_jwt(
             payload={
                 "user_id": user.id,
@@ -103,6 +189,7 @@ class UserService:
             access_token=access_token,
             refresh_token=refresh_token,
         )
+        # TODO DRY END! =====================
 
 
 class JWTService:
