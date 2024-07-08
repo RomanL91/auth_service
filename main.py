9631b9e8f70bfdaf16de9e7e20c05e6699f7a941
hays0503 <cthtufgbv@mail.ru>
2024-07-08 18:06:44 +0500

    Разрешил CORS для работы с API (localhost/localhost:3000)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import uvicorn
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from core.settings import settings
 
@@ -8,6 +10,16 @@ from api_v1 import router as router_v1
 
 
 app = FastAPI()
+
+origins = ['http://localhost','http://localhost:3000']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 
@@ -18,4 +30,8 @@ async def start_test():
 
 
 if __name__ == "__main__":
+<<<<<<< HEAD
     uvicorn.run("main:app", port=8001, reload=True)
+=======
+    uvicorn.run("main:app", host="0.0.0.0", port=5001, reload=False)
+>>>>>>> ba81d64 (Разрешил CORS для работы с API)
