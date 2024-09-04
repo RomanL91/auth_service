4df7aeb6026b4b827379a307b3d2e094fb7d3d2b
RomanL91 <dev_roman91@mail.ru>
2024-09-04 16:49:16 +0500

    сущность для записи смс кодов

---
+++ sms_app/models.py
@@ -0,0 +1,25 @@
+import uuid
+
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from core import Base
+
+
+class SMSCode(Base):
+    code: Mapped[str] = mapped_column(
+        String(6), 
+        nullable=False,
+    )
+    is_used: Mapped[bool] = mapped_column(
+        default=True,
+    )
+    phone_number_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), 
+        ForeignKey('phonenumbers.id'),
+    )
+    phone_number: Mapped["PhoneNumbers"] = relationship( # type: ignore
+        "PhoneNumbers", 
+        back_populates="sms_codes",
+    )
