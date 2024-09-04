37615151ae2c27eaa183f145f4cd1f62c3280411
RomanL91 <dev_roman91@mail.ru>
2024-09-04 16:28:13 +0500

    создание сущности email

---
+++ email_app/models.py
@@ -0,0 +1,28 @@
+import uuid
+
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from core import Base
+
+
+class Email(Base):
+    email: Mapped[str] = mapped_column(
+        String, 
+        nullable=True, 
+    )
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), 
+        ForeignKey('userts.id'),
+    )
+    user: Mapped["Userts"] = relationship( # type: ignore
+        "Userts", 
+        back_populates="emails",
+    )
+
+    def __str__(self) -> str:
+        return f"id={self.id} email={self.email!r}"
+    
+    def __repr__(self) -> str:
+        return str(self)
