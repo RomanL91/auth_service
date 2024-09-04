2fa5e4c9b8bedd95296d899aceca849a6b97b674
RomanL91 <dev_roman91@mail.ru>
2024-09-04 13:40:18 +0500

    базовая модель создает ИД типа uud4 + всем унаследованным врея создания записи

--- core/BASE_model.py
+++ core/BASE_model.py
@@ -1,9 +1,14 @@
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import (
     DeclarativeBase,
     Mapped,
     declared_attr,
     mapped_column,
 )
+from datetime import datetime
+
+from core.function_utils import get_current_time
 
 
 class Base(DeclarativeBase):
@@ -13,4 +18,12 @@ class Base(DeclarativeBase):
     def __tablename__(cls) -> str:
         return f"{cls.__name__.lower()}s"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[str] = mapped_column(
+        UUID,
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        default=get_current_time,
+        nullable=True,
+    )
