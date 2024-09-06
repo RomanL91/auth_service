1fc3c498bac53b531b59079d65f5704028f7520c
RomanL91 <dev_roman91@mail.ru>
2024-09-06 12:47:39 +0500

    схема для сохранения SocialAccount

--- social_acc_app/schemas.py
+++ social_acc_app/schemas.py
@@ -1,6 +1,8 @@
 from typing import Annotated
 from annotated_types import MinLen, MaxLen
-from pydantic import BaseModel, EmailStr, ConfigDict, HttpUrl
+from pydantic import BaseModel, EmailStr, ConfigDict, HttpUrl, UUID4
+
+
 
 
 class OAuth2GoogleUrl(BaseModel):
@@ -19,3 +21,22 @@ class GoogleUserInfo(BaseModel):
     given_name: str
     family_name: str
     picture: HttpUrl
+
+
+# from user_app.schemas import SaveUserSchema
+# from email_app.schemas import SaveEmailSchema # a circular import
+
+class SocialAccountSchema(BaseModel):
+    provider: str
+    provider_user_id: str
+    user_id: UUID4
+    email_id: UUID4
+
+    @classmethod
+    def convert_data(cls, google_user: GoogleUserInfo, user, email) -> "SocialAccountSchema":
+        return cls(
+            provider="google",
+            provider_user_id=google_user.id,
+            user_id=user.id,
+            email_id=email.id
+        )
