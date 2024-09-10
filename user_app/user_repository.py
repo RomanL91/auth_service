1b078d23f0aac506adf1912341db02efa57771f2
RomanL91 <dev_roman91@mail.ru>
2024-09-11 02:03:44 +0500

    метод репоззитория пользователей для поиска пользователей по почтовому ящику

--- user_app/user_repository.py
+++ user_app/user_repository.py
@@ -1,6 +1,21 @@
+from sqlalchemy.future import select
+from sqlalchemy.orm import selectinload
+
 from user_app.models import UserT
+from email_app.models import Email
 from core.BASE_repository import SQLAlchemyRepository
 
 
 class UserRepository(SQLAlchemyRepository):
     model = UserT
+
+    async def get_user_by_email(self, email: str):
+        stmt = (
+            select(UserT)
+            .join(Email)
+            .filter(Email.email == email)
+            .options(selectinload(UserT.emails))  # Загрузка связанных email записей
+        )
+        result = await self.session.execute(stmt)
+        user = result.scalar_one_or_none()  # Возвращает одного пользователя или None
+        return user
