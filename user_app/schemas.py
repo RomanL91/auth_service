994c6d5975d0c6df4d01cae6cf394faae18605e0
RomanL91 <dev_roman91@mail.ru>
2024-09-17 12:35:23 +0500

    обновление схем для приложения пользователей

--- user_app/schemas.py
+++ user_app/schemas.py
@@ -2,10 +2,12 @@ from uuid import uuid4
 from typing import Annotated
 from pydantic import BaseModel, ConfigDict, Field, UUID4
 
-from social_acc_app.schemas import GoogleUserInfo
-
 
 class SaveUserSchema(BaseModel):
+    model_config = ConfigDict(
+        strict=True,
+        from_attributes=True,
+    )
     id: UUID4
 
 
@@ -20,24 +22,10 @@ class User(BaseModel):
             description="Уникальный ID пользователя.",
         )
     ] = uuid4()
-    firt_name: Annotated[str | None, Field(..., description="Имя пользователя.")] = None
+    first_name: Annotated[str | None, Field(..., description="Имя пользователя."),] = None
     last_name: Annotated[
-        str | None, Field(..., description="Фамилия пользователя.")
+        str | None, Field(..., description="Фамилия пользователя."),
     ] = None
-    # updated_at: Annotated[
-    #     datetime | None,
-    #     Field(
-    #         ...,
-    #         description="Время обновления пользователя в системе.",
-    #     ),
-    # ] = None
-    # last_login: Annotated[
-    #     datetime | None,
-    #     Field(
-    #         ...,
-    #         description="Время последнего логина в системе.",
-    #     ),
-    # ] = None
     active: Annotated[
         bool | None,
         Field(
@@ -45,35 +33,3 @@ class User(BaseModel):
             description="Статус активности пользователя.",
         ),
     ] = True
-    # external_id: Annotated[
-    #     int | str | None,
-    #     Field(
-    #         ...,
-    #         description="Уникальный ID пользователя для интеграций с другими системами.",
-    #     ),
-    # ] = None
-    # client_uuid: Annotated[
-    #     UUID4,
-    #     Field(
-    #         ...,
-    #         description="Уникальный ID с клиента",
-    #     ),
-    # ] = None
-    # phone_number_id: Annotated[
-    #     UUID4,
-    #     Field(
-    #         ...,
-    #         description="Внешний ключ таблицы телефонных номеров.",
-    #     ),
-    # ] = None
-    # phone_number: Optional[PhoneNumber] = None
-    # emails: List[Email] = []
-    # social_accounts: List[SocialAccount] = []
-    # tokens: List[JWToken] = []
-
-    @classmethod
-    def from_google_info(cls, google_user: GoogleUserInfo) -> "User":
-        return cls(
-            firt_name=google_user.given_name,
-            last_name=google_user.family_name,
-        )
