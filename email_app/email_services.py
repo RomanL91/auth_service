004d07b52d3e95c55b5b9a1a5459b7ced75de9bd
RomanL91 <dev_roman91@mail.ru>
2024-09-17 12:40:57 +0500

    схемы и метод для работы с данными после адаптации

--- email_app/email_services.py
+++ email_app/email_services.py
@@ -3,19 +3,30 @@ from core.BASE_unit_of_work import IUnitOfWork
 
 # == Exceptions
 from fastapi import HTTPException, status
-from sqlalchemy.exc import IntegrityError, NoResultFound
+from sqlalchemy.exc import IntegrityError
 
 # == Schemas
 from email_app.schemas import EmailSchema
-from social_acc_app.schemas import GoogleUserInfo
-from user_app.schemas import SaveUserSchema
+from social_acc_app.schemas import DataUserForMyService
 
 
 class EmailService:
+    exclude = (
+        "id",
+        "first_name",
+        "last_name",
+        "provider",
+        "provider_user_id",
+        "ava",
+        "active",
+    )
     async def create_email(
-        self, uow: IUnitOfWork, google_user: GoogleUserInfo, user: SaveUserSchema
+        self, uow: IUnitOfWork, data_user: DataUserForMyService, **kwargs,
     ) -> EmailSchema | None:
-        email_dict = EmailSchema.convert_data(google_user, user).model_dump()
+        email_dict = {
+            **data_user.model_dump(exclude=self.exclude),
+            **kwargs
+        }
         async with uow:
             try:
                 email = await uow.email.create_obj(email_dict)
--- email_app/schemas.py
+++ email_app/schemas.py
@@ -1,24 +1,6 @@
-from typing import Annotated
-from annotated_types import MinLen, MaxLen
-from pydantic import BaseModel, EmailStr, ConfigDict, HttpUrl, UUID4
-
-from social_acc_app.schemas import GoogleUserInfo
-from user_app.schemas import SaveUserSchema
-
-
-class SaveEmailSchema(BaseModel):
-    id: UUID4
+from pydantic import BaseModel, EmailStr, UUID4
 
 
 class EmailSchema(BaseModel):
     email: EmailStr
     user_id: UUID4
-
-    @classmethod
-    def convert_data(
-        cls, google_user: GoogleUserInfo, user: SaveUserSchema
-    ) -> "EmailSchema":
-        return cls(
-            email=google_user.email,
-            user_id=user.id,
-        )
