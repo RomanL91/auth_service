c3795032986aa45712879680f081da51bfc575d9
RomanL91 <dev_roman91@mail.ru>
2024-09-17 12:34:17 +0500

    новый метод репозитория пользователей для проверки на существование

--- user_app/user_repository.py
+++ user_app/user_repository.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import selectinload
 
 from user_app.models import UserT
 from email_app.models import Email
+from social_acc_app.models import SocialAccount
 from core.BASE_repository import SQLAlchemyRepository
 
 
@@ -18,4 +19,29 @@ class UserRepository(SQLAlchemyRepository):
         )
         result = await self.session.execute(stmt)
         user = result.scalar_one_or_none()  # Возвращает одного пользователя или None
+        return user
+    
+    async def get_user_by_social_id(self, social_id: str):
+        stmt = (
+            select(UserT)
+            .join(SocialAccount)
+            .filter(SocialAccount.provider_user_id == social_id)
+            .options(selectinload(UserT.social_accounts))  # Загрузка связанных social_accounts записей
+        )
+        result = await self.session.execute(stmt)
+        user = result.scalar_one_or_none()  # Возвращает одного пользователя или None
+        return user
+    
+    async def get_user_by_social_or_email(self, email: str = None, provider_user_id: str = None):
+        # Строим запрос к таблице пользователей с объединением email и social_accounts
+        stmt = (
+            select(UserT)
+            .join(Email)  # Соединяем таблицу email
+            .join(SocialAccount)  # Соединяем таблицу социальных аккаунтов
+            .filter((Email.email == email) | (SocialAccount.provider_user_id == provider_user_id))  # Применяем фильтры
+            .options(selectinload(UserT.emails), selectinload(UserT.social_accounts))  # Загружаем связанные сущности
+        )
+        # Выполняем запрос
+        result = await self.session.execute(stmt)
+        user = result.scalar_one_or_none()  # Возвращаем одного пользователя или None
         return user
