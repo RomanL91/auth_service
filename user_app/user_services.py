3554745aed42889e07c6d35860ca41b25e6ca975
RomanL91 <dev_roman91@mail.ru>
2024-09-17 12:33:25 +0500

    частичный рефактор сервиса пользователей

--- user_app/user_services.py
+++ user_app/user_services.py
@@ -6,23 +6,45 @@ from core.BASE_unit_of_work import IUnitOfWork
 
 # == Exceptions
 from fastapi import HTTPException, status
-from sqlalchemy.exc import IntegrityError, NoResultFound
+from sqlalchemy.exc import IntegrityError
 
 # == Schemas
-from user_app.schemas import User, SaveUserSchema
-from social_acc_app.schemas import CodeFromGoogle, GoogleUserInfo
+from user_app.schemas import User
+from social_acc_app.schemas import GoogleForm, VKForm, DataUserForMyService
 from jwt_app.schemas import JWTokensResponse
 
+# == Services
+from jwt_app.jwt_services import JWTService
 from email_app.email_services import EmailService
 from social_acc_app.social_services import SocialService
-from jwt_app.jwt_services import JWTService
+
+from user_app.adapter import UserFactoryAdapter
 
 
 class UserService:
+    # TODO Внедрение Unit of Work на уровне сервиса
+    # def __init__(self, uow: IUnitOfWork):
+        # self.uow = uow
+    # Вместо того чтобы передавать uow в каждый метод, можно внедрить uow в сам сервис, 
+    # а управление транзакциями осуществлять на уровне класса сервиса. 
+    # Это уменьшит количество передаваемых параметров
+    
+    exclude = ("email", "ava", "provider_user_id", "provider")
+
+    async def get_user_by_socia_id(self, uow: IUnitOfWork, social_id: str) -> User:
+        async with uow:
+            return await uow.user.get_user_by_social_id(social_id)
+        
+    async def get_user_by_social_or_email(self, uow: IUnitOfWork, email: str = None, provider_user_id: str = None):
+        async with uow:
+            return await uow.user.get_user_by_social_or_email(email, provider_user_id)
+
     async def create_user(
-        self, uow: IUnitOfWork, new_user: User
-    ) -> SaveUserSchema | None:
-        user_dict = User.from_google_info(new_user).model_dump()
+        self, uow: IUnitOfWork, new_user: DataUserForMyService
+    ) -> User | None:
+        user_dict = new_user.model_dump(
+            exclude=self.exclude
+        )
         async with uow:
             try:
                 user = await uow.user.create_obj(user_dict)
@@ -34,59 +56,119 @@ class UserService:
                     detail=e,  # TODO пока что показываем ошибки
                 )
 
-    async def get_user_info_from_google(
-        self, code: CodeFromGoogle
-    ) -> GoogleUserInfo | None:
-        async with httpx.AsyncClient() as client:
-            token_response = await client.post(
-                settings.google_auth.google_token_url,
-                data=settings.google_auth.get_data_to_post(code.code),
-            )
-            token_data = token_response.json()
-            access_token = token_data.get("access_token", False)
-
-            # Если токен получен успешно
-            if access_token:
-                # Получаем информацию о пользователе с помощью access_token
-                user_info_response = await client.get(
-                    settings.google_auth.google_user_info_url,
-                    headers=settings.google_auth.get_headers(access_token),
-                )
-                user_info = GoogleUserInfo(**user_info_response.json())
-                return user_info
-            else:
-                return None
-
-    async def get_user_by_email(self, uow: IUnitOfWork, email: str) -> User:
-        async with uow:
-            return await uow.user.get_user_by_email(email)
-
-    async def auth_google(self, uow: IUnitOfWork, data: CodeFromGoogle) -> JWTokensResponse:
-        # может это паттерн цепочка обязанностей?
-        data_user_google = await UserService().get_user_info_from_google(data)
-        user = await self.get_user_by_email(uow=uow, email=data_user_google.email)
+    async def auth_google(self, uow: IUnitOfWork, data_google_form: GoogleForm) -> JWTokensResponse:
+        data_form_google = data_google_form.model_dump()
+        settings.google_auth.data_post = data_form_google
+        access_token_google = await self.get_access_key_from_oauth_service(
+            url=settings.google_auth.google_token_url,
+            payload_template=settings.google_auth.data_post,
+        )
+        # получение инфы о пользователе
+        data_user_from_google = await UserFactoryAdapter.fetch_user_info(
+            url=settings.google_auth.google_user_info_url,
+            headers=settings.google_auth.get_headers(access_token_google)
+        )
+        # адаптирую для БД сервиса
+        customized_user_data = UserFactoryAdapter.create_user(
+            data=data_user_from_google, source="google"
+        )
+        # проверка на существование
+        user = await self.get_user_by_social_or_email(
+            uow=uow,
+            email=data_user_from_google.email,
+            provider_user_id=data_user_from_google.id
+        )
         if user is not None:
             jwt = await JWTService().create_jwt(
                 uow=uow,
                 user=user,
             )
             return jwt
-        user = await UserService().create_user(
-            uow=uow, new_user=data_user_google
+        user = await self.create_user(
+            uow=uow, new_user=customized_user_data
         )
         email = await EmailService().create_email(
             uow=uow,
-            google_user=data_user_google,
-            user=user,
+            data_user=customized_user_data,
+            user_id=user.id,
         )
         social = await SocialService().create_social_acc(
             uow=uow,
-            google_user=data_user_google,
-            user=user,
-            email=email,
+            data_user=customized_user_data,
+            user_id=user.id,
+            email_id=email.id,
         )
         jwt = await JWTService().create_jwt(
             uow=uow,
             user=user,
         )
         return jwt
+
+    async def auth_vk(self, uow: IUnitOfWork, params: VKForm) -> JWTokensResponse:
+        data_form_vk = params.model_dump()
+        settings.vk_auth.data_post_request_to_receive_keys = data_form_vk
+        test_access_vk = await self.get_access_key_from_oauth_service(
+            url=settings.vk_auth.vk_token_url,
+            headers=settings.vk_auth.headers,
+            payload_template=settings.vk_auth.data_post_request_to_receive_keys,
+        )
+        # получение инфы о пользователе
+        test_data_user = await UserFactoryAdapter.fetch_user_info(
+            url=settings.vk_auth.vk_user_info_url,
+            data=settings.vk_auth.get_data_payload(test_access_vk),
+            request_method="POST",
+        )
+        # адаптирую для БД сервиса
+        customized_user_data = UserFactoryAdapter.create_user(
+            data=test_data_user, source="vk"
+        )
+        # проверка на существование
+        user = await self.get_user_by_social_or_email(
+            uow=uow,
+            email=test_data_user.email,
+            provider_user_id=test_data_user.user_id
+        )
+        if user is not None:
+            jwt = await JWTService().create_jwt(
+                uow=uow,
+                user=user,
+            )
+            return jwt
+        user = await self.create_user(
+            uow=uow, new_user=customized_user_data
+        )
+        email = await EmailService().create_email(
+            uow=uow,
+            data_user=customized_user_data,
+            user_id=user.id,
+        )
+        social = await SocialService().create_social_acc(
+            uow=uow,
+            data_user=customized_user_data,
+            user_id=user.id,
+            email_id=email.id,
+        )
+        jwt = await JWTService().create_jwt(
+            uow=uow,
+            user=user,
+        )
+        return jwt
+
+
+    async def get_access_key_from_oauth_service(
+        self,
+        url: str,
+        payload_template: dict,
+        headers: dict = {},
+    ):
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                url=url,
+                headers=headers,
+                data=payload_template,
+            )
+            response_data = response.json()
+            access_token = response_data.get("access_token", False)
+            
+            if access_token:
+                return access_token
