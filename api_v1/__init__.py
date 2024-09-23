c776d836215882b805a539cee00d665779f75950
RomanL91 <dev_roman91@mail.ru>
2024-09-23 16:12:23 +0500

    подключение роутов

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from .oauth2_google.views import router as oath2_google_router
 from .oauth_vk.views import router as oauth2_vk_router
+from .auth_phone.views import router as auth_phone_router
 
 
 router = APIRouter()
 
 router.include_router(router=oath2_google_router, prefix="/auth_user")
 router.include_router(router=oauth2_vk_router, prefix="/auth_user")
+router.include_router(router=auth_phone_router, prefix="/auth_user")
