d3ed3a14f67634bf42dac972f8ff3e2f66973cf7
RomanL91 <dev_roman91@mail.ru>
2024-09-23 16:05:08 +0500

    слой сервиса sms_app

---
+++ sms_app/sms_service.py
@@ -0,0 +1,12 @@
+from core.BASE_unit_of_work import IUnitOfWork
+
+
+class SMSService:
+
+    async def create_sms_code(self, uow: IUnitOfWork, data):
+        async with uow:
+            sms_code = await uow.sms.create_obj(
+                data
+            )
+            await uow.commit()
+            return sms_code
