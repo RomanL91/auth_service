0a6c56eeab3763d763118b9fffe2675d35b87404
RomanL91 <dev_roman91@mail.ru>
2024-09-25 15:13:32 +0500

    методы для аутентификации через смс

--- sms_app/sms_service.py
+++ sms_app/sms_service.py
@@ -1,5 +1,7 @@
 from core.BASE_unit_of_work import IUnitOfWork
 
+from jwt_app.jwt_services import JWTService
+
 
 class SMSService:
 
@@ -8,3 +10,26 @@ class SMSService:
             sms_code = await uow.sms.create_obj(data)
             await uow.commit()
             return sms_code
+        
+    
+    async def chec_sms_code(self, uow: IUnitOfWork, code: str, phone_number_id: str, is_used: bool = True,):
+        async with uow:
+            code = await uow.sms.get_obj(
+                code=code,
+                phone_number_id=phone_number_id,
+                is_used=is_used,
+            )
+        return code
+    
+
+    async def mark_code_as_used_and_issued_tokens(self, uow: IUnitOfWork, sms_code_id: str,):
+        async with uow:
+            code = await uow.sms.update_obj(sms_code_id, {"is_used": False})
+            user = await uow.user.get_obj(
+                phone_number_id=code.phone_number_id,
+            )
+            jwt = await JWTService().create_jwt(
+                uow=uow, user=user
+            )
+            await uow.commit()
+            return jwt
