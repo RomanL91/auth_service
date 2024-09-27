114312c6bdc31f2752c09af23b6b2ce1ef90b92a
RomanL91 <dev_roman91@mail.ru>
2024-09-27 23:52:34 +0500

    подключение роуторов

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -3,10 +3,13 @@ from fastapi import APIRouter
 from .oauth2_google.views import router as oath2_google_router
 from .oauth_vk.views import router as oauth2_vk_router
 from .auth_phone.views import router as auth_phone_router
-
+from .user.views import router as user_router
+from .jwt.views import router as jwt_router
 
 router = APIRouter()
 
 router.include_router(router=oath2_google_router, prefix="/auth_user")
 router.include_router(router=oauth2_vk_router, prefix="/auth_user")
 router.include_router(router=auth_phone_router, prefix="/auth_user")
+router.include_router(router=user_router, prefix="/user")
+router.include_router(router=jwt_router, prefix="/token")
