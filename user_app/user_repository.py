3557667d3d54ea9e33a7c140cd8f41b392aa37d0
RomanL91 <dev_roman91@mail.ru>
2024-09-27 23:44:13 +0500

    метод репозитория пользователя для получения полной информации о пользователе

--- user_app/user_repository.py
+++ user_app/user_repository.py
@@ -54,3 +54,57 @@ class UserRepository(SQLAlchemyRepository):
         # Выполняем запрос
         result = await self.session.execute(stmt)
         return result.scalar_one_or_none()
+
+    async def get_user_details(self, user_id: str):
+        try:
+            stmt = (
+                select(UserT)
+                .where(UserT.id == user_id)
+                .options(
+                    selectinload(UserT.emails),
+                    selectinload(UserT.social_accounts),
+                    selectinload(UserT.phone_number),
+                )
+            )
+
+            # Выполняем запрос и получаем результат
+            result = await self.session.execute(stmt)
+            user = result.scalar_one_or_none()
+
+            if user is None:
+                raise ValueError("Пользователь не найден")
+
+            # Форматируем результат в словарь или другую структуру
+            # у меня есть UserDetailSchema и по сути результат запроса в БД
+            # можно сразу преобразовывать и валидировать в эту модель, но я 
+            # решил делать это уровнем позже, а тут возвращать словарь
+            user_data = {
+                "user": {
+                    "id": user.id,
+                    "first_name": user.first_name,
+                    "last_name": user.last_name,
+                    "last_login": user.last_login,
+                    "active": user.active,
+                    "external_id": user.external_id,
+                    "client_uuid": user.client_uuid,
+                },
+                "emails": [{"id": email.id, "email": email.email} for email in user.emails],
+                "social_accounts": [
+                    {
+                        "id": sa.id,
+                        "provider": sa.provider,
+                        "provider_user_id": sa.provider_user_id,
+                    }
+                    for sa in user.social_accounts
+                ],
+                "phone_number": {
+                    "id": user.phone_number.id if user.phone_number else None,
+                    "phone_number": user.phone_number.phone_number if user.phone_number else None,
+                }
+            }
+
+            return user_data
+
+        except Exception as e:
+            print(f"Ошибка при получении информации о пользователе: {e}")
+            return None
